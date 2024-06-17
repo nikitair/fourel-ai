6a24c3a26ec2614dd1e98f5423290cd3223ffd7d
Nikita Stoliarov <stlrvnk@gmail.com>
2024-06-17 12:22:24 +0300

    Add mailhook router

---
+++ src/mailhook/routers.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Request
+from config.logging_config import logger
+
+
+router = APIRouter()
+
+
+@router.get("/")
+async def mailhook_index():
+    logger.info("*** MAILHOOK INDEX TRIGGERED")
+    return {"service": "Fourel AI", "router": "Mailhook"}
+
+
+@router.post("/echo")
+async def mailhook_echo(request: Request):
+    logger.info("*** MAILHOOK ECHO TRIGGERED")
+    return request
+
+
+@router.post("/")
+async def mailhook_echo(request: Request):
+    logger.info("*** MAILHOOK TRIGGERED")
+    return {"messages": "Under Development"}
--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
 import uvicorn
+from config.logging_config import logger
+from mailhook.routers import router as mailhook_router
 
 
 app = FastAPI()
 
+# register routers
+app.include_router(router=mailhook_router, prefix="mailhook", tags=["mailhook"])
+
 
 @app.get("/")
 async def index():
+    logger.info("*** FOUREL AI INDEX TRIGGERED")
     return {"service": "Fourel AI"}
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8081, reload=True)
